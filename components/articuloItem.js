a96d44917459c904593d8fb87f67fbf3298c1ef0
84325JuanRencini <juanirencin@gmail.com>
2024-07-18 16:30:55 -0300

    Terminada la pagina 'item' con su funcionalidad y agregado de responsividad

--- components/articuloItem.js
+++ components/articuloItem.js
@@ -1,14 +1,11 @@
 export const itemComponent = (title, img, text, price, fecha) => {
   const formattedPrice = price.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' });
 
-  return `     
-      <div class="col-md-8 mx-auto">
-        <div class="card">
-          <div class="card-body">
+  return `
             <div class="row">
               <div class="col-md-7">
                 <img src="${img}" alt="" 
-                class="img" width="700px" height="700px" >
+                class="img" class="img-fluid" style="max-width: 100%; height: auto;" >
               </div>
               <div class="col-md-5">
                 <h1>${title}</h1>
@@ -16,20 +13,6 @@ export const itemComponent = (title, img, text, price, fecha) => {
                 <h4 class="price">${formattedPrice}</h4>
                 <h5 class="mt-5 mx-auto">${text}</h5>
               </div>
-            </div>
-          </div>
-          <div class="card-footer">
-            <div class="d-flex ms-auto align-items-center justify-content-end">
-              <button id="btnCart" class="btn btn-success"> 
-                <h5>Agregar al carrito 
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-                  </svg>
-                </h5>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>`;
+            </div>`;
 };
   
--- private_page/item/item.js
+++ private_page/item/item.js
@@ -1,23 +1,27 @@
 import { itemComponent } from "../../components/articuloItem.js";
+import { getData, setData } from "../../utils/localStorageController.js";
 
-const itemContainer = document.getElementById('itemContainer')
+const itemContainer = document.getElementById('itemContainer');
+const btnCart = document.getElementById('btnCart');
 const url = new URL(window.location.href);
 const itemId = Number(url.searchParams.get('id'));
 
-console.log(url)
-console.log(itemId)
+let artTitle = null;
+let artImg = null;
+let artPrice = null;
 
 function LlenarItem(art) {
-  const item = itemComponent(art.titulo, art.imagen, art.text, art.price, art.fecha)
-  itemContainer.innerHTML = item
+  artTitle = art.titulo;
+  artImg = art.imagen;
+  artPrice = art.price;
+  const item = itemComponent(art.titulo, art.imagen, art.text, art.price, art.fecha);
+  itemContainer.innerHTML = item;
 }
 
 fetch('../../data/data.json')
   .then(res => res.json())
   .then(data => {
     let articuloEncontrado = null;
-
-    console.log(articuloEncontrado)
 
     data.forEach(cat => {
       const articulo = cat.articulos.find(e => e.id === itemId);
@@ -27,8 +31,27 @@ fetch('../../data/data.json')
     });
     if (articuloEncontrado) {
       LlenarItem(articuloEncontrado);
-      }
+    }
   })
   .catch(error => {
     console.error('Error al cargar el JSON:', error);
   });
+
+  
+btnCart.addEventListener('click', (event) => { 
+
+  if (confirm('¿Quiere agregar este artítulo al carrito?')) {
+    const items = getData('itemsData') || [];
+    const newIndex = items.length;   
+    const newCant = document.getElementById('ph-cantidad').value;
+    const newTotal = (artPrice * parseInt(newCant));
+
+    const newItem = { id: newIndex, title: artTitle, img: artImg, price: artPrice, cant: newCant, total: newTotal };
+
+    items.push(newItem);
+    setData('itemsData', items);
+    console.log('Item agregado:');
+
+    window.location.href = "../cart/carrito.html"
+  }
+});
